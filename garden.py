68863069514c46512dbe6b3af7b6a086ee4e6056
voquangchinhbtx-gif <voquangchinhbtx@gmail.com>
2026-03-15 08:40:43 +0700

    Update garden.py

--- garden.py
+++ garden.py
@@ -1,1 +1,25 @@
+import streamlit as st
+from datetime import date
+from database import add_plant, save_data
+from crop_database import CROPS
+
+
+def show_garden(data):
+
+    st.title("🌱 Quản lý vườn")
+
+    crop = st.selectbox("Chọn cây", list(CROPS.keys()))
+
+    plant_date = st.date_input("Ngày trồng", date.today())
+
+    if st.button("Thêm cây"):
+
+        add_plant(data, {
+            "crop": crop,
+            "date": plant_date.strftime("%Y-%m-%d")
+        })
+
+        save_data(data)
+
+        st.success("Đã thêm cây")
 
