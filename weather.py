92439399f9285ba7572bf648641e78d7d89fb7b4
voquangchinhbtx-gif <voquangchinhbtx@gmail.com>
2026-03-15 08:38:33 +0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,1 +1,26 @@
+import requests
+from config import WEATHER_API, WEATHER_KEY
+
+
+def get_weather(lat, lon):
+
+    try:
+
+        url = f"{WEATHER_API}?lat={lat}&lon={lon}&appid={WEATHER_KEY}&units=metric"
+
+        r = requests.get(url).json()
+
+        return {
+            "temp": r["main"]["temp"],
+            "hum": r["main"]["humidity"],
+            "wind": r["wind"]["speed"]
+        }
+
+    except:
+
+        return {
+            "temp": 30,
+            "hum": 70,
+            "wind": 2
+        }
 
