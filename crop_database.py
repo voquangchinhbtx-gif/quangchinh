629f49245b1bbd6716e815ffe05ec0f4c995a826
voquangchinhbtx-gif <voquangchinhbtx@gmail.com>
2026-03-15 08:38:59 +0700

    Update crop_database.py

--- crop_database.py
+++ crop_database.py
@@ -1,1 +1,26 @@
+CROPS = {
+
+    "chili": {
+        "name": "Ớt",
+        "npk": "16-16-8"
+    },
+
+    "tomato": {
+        "name": "Cà chua",
+        "npk": "15-15-15"
+    },
+
+    "cucumber": {
+        "name": "Dưa leo",
+        "npk": "20-20-20"
+    }
+}
+
+
+def get_crop_info(crop):
+
+    return CROPS.get(crop, {
+        "name": crop,
+        "npk": "?"
+    })
 
