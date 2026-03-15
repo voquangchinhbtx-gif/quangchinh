f9e5178c8d395ab0b075d1eecf32244163aef4f5
voquangchinhbtx-gif <voquangchinhbtx@gmail.com>
2026-03-15 08:39:25 +0700

    Update npk_ai.py

--- npk_ai.py
+++ npk_ai.py
@@ -1,1 +1,23 @@
+import numpy as np
+from PIL import Image
+
+
+def analyze_leaf_npk(image):
+
+    img = np.array(image)
+
+    r = img[:, :, 0].mean()
+    g = img[:, :, 1].mean()
+    b = img[:, :, 2].mean()
+
+    if g < 80:
+        return "Thiếu Đạm (N)"
+
+    if r > 150 and g < 120:
+        return "Thiếu Lân (P)"
+
+    if b > 140:
+        return "Thiếu Kali (K)"
+
+    return "Lá khỏe - dinh dưỡng bình thường"
 
