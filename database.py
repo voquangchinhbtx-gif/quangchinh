896d8bea1dc374c41dc97e6eec539c9f17c61135
voquangchinhbtx-gif <voquangchinhbtx@gmail.com>
2026-03-15 08:37:56 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,38 @@
+import json
+import os
+from config import DATA_FILE
+
+
+def load_data():
+
+    if not os.path.exists(DATA_FILE):
+
+        data = {"plants": []}
+
+        with open(DATA_FILE, "w") as f:
+            json.dump(data, f)
+
+    with open(DATA_FILE) as f:
+        return json.load(f)
+
+
+def save_data(data):
+
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f, indent=2)
+
+
+def add_plant(data, plant):
+
+    data["plants"].append(plant)
+
+
+def delete_plant(data, index):
+
+    data["plants"].pop(index)
+
+
+def get_plants(data):
+
+    return data["plants"]
 
