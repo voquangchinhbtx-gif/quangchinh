dd1071f43d59b11e93bc905c02970bbefc291509
voquangchinhbtx-gif <voquangchinhbtx@gmail.com>
2026-03-15 08:39:56 +0700

    Update gemini_ai.py

--- gemini_ai.py
+++ gemini_ai.py
@@ -1,1 +1,26 @@
+import google.generativeai as genai
+from config import GEMINI_MODEL
+
+
+def load_model(api):
+
+    genai.configure(api_key=api)
+
+    return genai.GenerativeModel(GEMINI_MODEL)
+
+
+def diagnose(model, image):
+
+    prompt = """
+Phân tích bệnh cây trong ảnh.
+Cho biết:
+
+- tên bệnh
+- nguyên nhân
+- cách xử lý
+"""
+
+    response = model.generate_content([prompt, image])
+
+    return response.text
 
